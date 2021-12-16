1a8d3c203545914db75a791f9e4e1ec6b9ed6bf5
Zach Smith <Lemelisk27@gmail.com>
2021-12-16 02:21:05 -0800

    added middleware and more user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,6 +4,7 @@ const {User} = require("../../models")
 const sequelize = require("../../config/connection")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
+const tokenAuth = require("../../middleware/tokenAuth")
 
 router.get("/", (req, res) => {
     User.findAll({
@@ -47,6 +48,70 @@ router.post("/login", (req, res) => {
             res.json({
                 token: token,
                 user: foundUser
+            })
+        }
+        else {
+            res.status(401).json({Message: "Incorrect Username or Password"})
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.put("/", tokenAuth, (req, res) => {
+    User.update({
+        about: req.body.about,
+        city: req.body.city,
+        email: req.body.email,
+        first_name: req.body.first_name,
+        github: req.body.github,
+        last_name: req.body.last_name,
+        linkedin: req.body.linkedin,
+        phone: req.body.phone,
+        portfolio: req.body.portfolio,
+        state: req.body.state,
+        street: req.body.street,
+        zip: req.body.zip
+    },
+    {
+        where: {
+            id: req.body.id,
+        }
+    })
+    .then(updatedUser=>{
+        res.json(updatedUser)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.post("/password", tokenAuth, (req, res) => {
+    const newPassword = bcrypt.hashSync(req.body.new_password,10)
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(foundUser=>{
+        if (bcrypt.compareSync(req.body.password,foundUser.password)) {
+            User.update({
+                password: newPassword
+            },
+            {
+                where: {
+                    id: foundUser.id
+                }
+            })
+            .then(updatedPwd=>{
+                res.json(updatedPwd)
+            })
+            .catch(err=>{
+                console.log(err)
+                res.status(500).json({Message: "An Error Occured", err:err})
             })
         }
         else {
